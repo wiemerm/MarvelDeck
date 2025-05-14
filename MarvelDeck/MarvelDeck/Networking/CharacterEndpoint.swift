fb58a4fb337e7f3b4aea73120f6593a9a9033843
Megan Wiemer <megan.wiemer@gmail.com>
2025-05-14 14:47:23 -0500

    Swap for using provided api key info

--- MarvelDeck/MarvelDeck/Networking/CharacterEndpoint.swift
+++ MarvelDeck/MarvelDeck/Networking/CharacterEndpoint.swift
@@ -24,24 +24,31 @@ enum CharacterEndpoint: Endpoint {
     }
 
     var queryItems: [URLQueryItem] {
-//        let timestamp = "\(Int(Date().timeIntervalSince1970))"
-//              let apiKey = EnvironmentVariables.publicKey
-//              let privateKey = EnvironmentVariables.privateKey
-//              let hashString = (timestamp + privateKey + apiKey)
-//
-//        let digest = Insecure.MD5.hash(data: Data(hashString.utf8))
-//
-//        let hash =  digest.map {
-//            String(format: "%02hhx", $0)
-//        }.joined()
+        // Setting up hash using CryptoKit results in "The passed API key is invalid."
+        let timestamp = Int(Date().timeIntervalSince1970).description
+        let publicKey = EnvironmentVariables.publicKey
+        let privateKey = EnvironmentVariables.privateKey
+        let hashString = timestamp + privateKey + publicKey
 
-        let timestamp = "1670913383902"
-        let apiKey = "edc9531ea872c74a2855ed93a5903229"
-        let hash = "bbb581dcf34e4752243b361daa960fb1"
+        guard let data = hashString.data(using: .utf8) else {
+            return []
+        }
+
+        let digest = Insecure.MD5.hash(data: data)
+
+        let hash =  digest.map {
+            String(format: "%02hhx", $0)
+        }.joined()
+
+        // Found from https://github.com/9Yogesh9/Marvel-API/blob/main/assests/scripts/script.js
+        // and appears to be a working hash
+//        let timestamp = "1670913383902"
+//        let publicKey = "edc9531ea872c74a2855ed93a5903229"
+//        let hash = "bbb581dcf34e4752243b361daa960fb1"
 
         var queryItems = [
             URLQueryItem(name: "ts", value: timestamp),
-            URLQueryItem(name: "apikey", value: apiKey),
+            URLQueryItem(name: "apikey", value: publicKey),
             URLQueryItem(name: "hash", value: hash)
         ]
 
